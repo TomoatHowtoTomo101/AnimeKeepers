6ed8b6e56d7d6769c7c9dbcdfa362383fed08305
iLaysChipz <toastedrice@gmail.com>
2019-05-28 02:42:55 -0600

    Mini Update
    Improved some animations and the code base for the template character. Started programming the camera to be dynamic.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -19,7 +19,7 @@ public class CameraScript : MonoBehaviour
         Vector3 displacement = target.position - transform.position;
         Vector3 hDispacement = new Vector3(displacement.x, 0, displacement.z);
         Vector3 facing = transform.rotation.eulerAngles;
-        float distance = displacement.magnitude;
+        float distance = hDispacement.magnitude;
         float hAngleDiff = Vector3.Angle(hDispacement, transform.rotation * Vector3.left) - 90;
         float vAngleDiff = Vector3.Angle(displacement, Vector3.up) - 90 - facing.x + (facing.x > 180 ? 360:0);
         float hFactor = Mathf.Abs(hAngleDiff / 200);
@@ -29,7 +29,7 @@ public class CameraScript : MonoBehaviour
 
         if (distance > 20) transform.position += hDispacement.normalized * (distance - 20);
         if (distance < 10) transform.position += hDispacement.normalized * (distance - 10);
-        transform.position += new Vector3(0, height - transform.position.y,0);
+        transform.position += new Vector3(0, distance/2 - transform.position.y,0);
         transform.rotation = Quaternion.Euler(vAngleDiff * vFactor + facing.x, hAngleDiff * hFactor + facing.y, 0);
         //transform.LookAt(target);
     }
